98c681f5a22a59ce40cce89072524028a8ec1935
valeserber <valserber@gmail.com>
2016-02-01 16:26:51 -0300

    added other payment methods

--- app/src/main/java/com/serber/valeria/paymentapp/activity/MainActivity.java
+++ app/src/main/java/com/serber/valeria/paymentapp/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.serber.valeria.paymentapp.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.app.AlertDialog;
 
 import com.serber.valeria.paymentapp.R;
 import com.serber.valeria.paymentapp.adapter.PaymentMethodAdapter;
@@ -25,15 +27,21 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
     private ProgressBar mLoadingView;
     private SwipeRefreshLayout  mRefreshView;
     private FrameLayout mErrorView;
+    private FrameLayout mFilterTypeView;
+    private FrameLayout mNoResultsView;
     private TextView mErrorMessage;
+    private TextView mFilterTypeText;
     private PaymentMethodAdapter mPaymentAdapter;
     private PaymentMethodProvider mProvider;
+    private PaymentType mCurrentFilter;
+    private AlertDialog mAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setUpLayout();
         initialize();
+        setUpAlertDialog();
         setListeners();
         startLoading();
     }
@@ -43,8 +51,12 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
         mLoadingView = (ProgressBar) findViewById(R.id.activity_main_loading);
         mRefreshView = (SwipeRefreshLayout) findViewById(R.id.activity_main_refresh_view);
         mErrorView = (FrameLayout) findViewById(R.id.error_view);
+        mFilterTypeView = (FrameLayout) findViewById(R.id.activity_main_filter_type);
+        mNoResultsView = (FrameLayout) findViewById(R.id.no_results_view);
         mErrorMessage = (TextView) findViewById(R.id.error_view_message);
+        mFilterTypeText = (TextView) findViewById(R.id.activity_main_filter_type_text);
         mErrorView.setVisibility(View.GONE);
+        mNoResultsView.setVisibility(View.GONE);
         mPaymentMethodView = (RecyclerView) findViewById(R.id.activity_main_payment_view);
         mPaymentAdapter = new PaymentMethodAdapter(this);
         mPaymentMethodView.setAdapter(mPaymentAdapter);
@@ -56,6 +68,35 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
             mProvider = new PaymentMethodProvider(getBaseContext());
         }
         mProvider.setView(this);
+        setFilter(PaymentType.CREDIT_CARD);
+    }
+
+    private void setUpAlertDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.select_payment_method));
+        builder.setItems(R.array.payment_methods, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                switch (item) {
+                    case 0:
+                        setFilter(PaymentType.CREDIT_CARD);
+                        break;
+                    case 1:
+                        setFilter(PaymentType.TICKET);
+                        break;
+                    case 2:
+                        setFilter(PaymentType.ATM);
+                        break;
+                    case 3:
+                        setFilter(PaymentType.DEBIT_CARD);
+                        break;
+                    case 4:
+                        setFilter(PaymentType.PREPAID_CARD);
+                        break;
+                }
+                startLoading();
+            }
+        });
+        mAlertDialog = builder.create();
     }
 
     private void setListeners() {
@@ -66,6 +107,17 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
                 loadData();
             }
         });
+        mFilterTypeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAlertDialog.show();
+            }
+        });
+    }
+
+    private void setFilter(PaymentType type) {
+        this.mCurrentFilter = type;
+        mFilterTypeText.setText(String.format(getString(R.string.filter), mCurrentFilter.getName()));
     }
 
     private void startLoading() {
@@ -83,15 +135,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
         mPaymentMethodView.setVisibility(View.GONE);
         mPaymentAdapter.clear();
         mErrorView.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void loadErrorView(String message) {
-        mRefreshView.setRefreshing(false);
-        mLoadingView.setVisibility(View.GONE);
-        mPaymentMethodView.setVisibility(View.GONE);
-        mErrorMessage.setText(message);
-        mErrorView.setVisibility(View.VISIBLE);
+        mNoResultsView.setVisibility(View.GONE);
     }
 
     private void loadData() {
@@ -99,16 +143,38 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
         mProvider.getPaymentMethods();
     }
 
+    private void hideMainView() {
+        mRefreshView.setRefreshing(false);
+        mLoadingView.setVisibility(View.GONE);
+        mPaymentMethodView.setVisibility(View.GONE);
+    }
+
+    public void loadNoResultsView() {
+        hideMainView();
+        mNoResultsView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void loadErrorView(String message) {
+        hideMainView();
+        mErrorMessage.setText(message);
+        mErrorView.setVisibility(View.VISIBLE);
+    }
+
     @Override
     public void filterResponse(List<PaymentMethod> list) {
         List<PaymentMethod> creditCardList = new ArrayList<>();
         for (PaymentMethod method: list) {
-            if (method.getPaymentTypeId().equals(PaymentType.CREDIT_CARD.getTypeId())) {
+            if (method.getPaymentTypeId().equals(mCurrentFilter.getTypeId())) {
                 creditCardList.add(method);
             }
         }
-        mPaymentAdapter.addResults(creditCardList);
-        finishLoading();
+        if (creditCardList.size() == 0) {
+            loadNoResultsView();
+        } else {
+            mPaymentAdapter.addResults(creditCardList);
+            finishLoading();
+        }
     }
 
     @Override
--- app/src/main/java/com/serber/valeria/paymentapp/model/PaymentType.java
+++ app/src/main/java/com/serber/valeria/paymentapp/model/PaymentType.java
@@ -1,21 +1,31 @@
 package com.serber.valeria.paymentapp.model;
 
+import com.serber.valeria.paymentapp.PaymentAppApplication;
+import com.serber.valeria.paymentapp.R;
+
 public enum PaymentType {
 
-    TICKET("ticket"),
-    ATM("atm"),
-    CREDIT_CARD("credit_card"),
-    DEBIT_CARD("debit_card"),
-    PREPAID_CARD("prepaid_card");
+    TICKET("ticket", PaymentAppApplication.getAppContext().getString(R.string.ticket)),
+    ATM("atm", PaymentAppApplication.getAppContext().getString(R.string.atm)),
+    CREDIT_CARD("credit_card", PaymentAppApplication.getAppContext().getString(R.string.credit_card)),
+    DEBIT_CARD("debit_card", PaymentAppApplication.getAppContext().getString(R.string.debit_card)),
+    PREPAID_CARD("prepaid_card", PaymentAppApplication.getAppContext().getString(R.string.prepaid_card));
 
     private String type_id;
+    private String name;
 
-    PaymentType(String type_id) {
+    PaymentType(String type_id, String name) {
         this.type_id = type_id;
+        this.name = name;
     }
 
     public String getTypeId() {
         return type_id;
     }
+
+    public String getName() {
+        return name;
+    }
+
 }
 
